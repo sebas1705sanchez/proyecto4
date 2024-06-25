a67d9934585e7764162c9ec541669dd1a48e71aa
PMarinPy <pablo.marin1@utp.edu.co>
2024-06-25 17:14:33 -0500

    new commit

--- tasklist/src/componentes/Task.js
+++ tasklist/src/componentes/Task.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../style-sheet/task.css"
+import { FaTrashCan } from "react-icons/fa6";
 
-function Task ( { text }) {
+function Task ( { text, completed}) {
   return (
-    <div className="task-cont">
+    <div className={completed ? "task-cont completed-task"}>
       <div className="task-text">
         {text}
       </div>
-      <div className="icon-task">
-        Delete
+      <div className="icon-task-cont">
+        <FaTrashCan className="icon-task" color="dark-blue"/>
       </div>
     </div>
   )
